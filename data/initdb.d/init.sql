2085767881a37ba289608c02884073ad723ec481
mborgerson <contact@mborgerson.com>
2025-08-26 01:07:32 -0700

    Update init.sql

--- data/initdb.d/init.sql
+++ data/initdb.d/init.sql
@@ -1,10 +1,9 @@
-CREATE DATABASE IF NOT EXISTS syncstorage_rs;
-CREATE DATABASE IF NOT EXISTS tokenserver_rs;
+-- create DBs (optional charset/collation recommended)
+CREATE DATABASE IF NOT EXISTS syncstorage_rs
+  CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS tokenserver_rs
+  CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-GRANT ALL PRIVILEGES
-  ON syncstorage_rs.*
-  TO sync;
-
-GRANT ALL PRIVILEGES
-  ON tokenserver_rs.*
-  TO sync;
+-- grant to the existing env-created user
+GRANT ALL PRIVILEGES ON syncstorage_rs.* TO 'sync'@'%';
+GRANT ALL PRIVILEGES ON tokenserver_rs.* TO 'sync'@'%';
